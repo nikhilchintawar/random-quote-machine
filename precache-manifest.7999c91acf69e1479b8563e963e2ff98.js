7bf7f2bea4f53a393c8ae6f7d54ad95fc732ad50
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2020-05-05 22:05:18 +0530

    Updates

---
+++ precache-manifest.7999c91acf69e1479b8563e963e2ff98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318f7bada64095e65f6317d1e0a5682a",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "d93125e6785a82355fb6",
+    "url": "/random-quote-machine/static/css/main.eae9dfa2.chunk.css"
+  },
+  {
+    "revision": "37859ecaec5b0b75a246",
+    "url": "/random-quote-machine/static/js/2.c16179c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.c16179c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93125e6785a82355fb6",
+    "url": "/random-quote-machine/static/js/main.5edbe0f8.chunk.js"
+  },
+  {
+    "revision": "52b3aacfd4b50869dda1",
+    "url": "/random-quote-machine/static/js/runtime-main.116f0ca4.js"
+  }
+]);
